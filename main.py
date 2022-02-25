6b7f751b9fa607402666cc09efa6f33d7448f1e2
=Szymon Jagaczewski <szymonjag2@gmail.com>
2022-02-25 12:35:46 +0100

    add a variable

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-import sys
+# import sys
+# import math
+#
+# print(sys.version)
+# a = 'zajęcia\n z wizualizacji danych'
+# print(a)
+# print(type(a))
+# b, c = 5.4, 5
+# print(b)
+# print(type(b))
+# print(c)
+# print(type(c))
+# d = 3 + 2j
+# print(d)
+# print(type(d))
+#
+# f = 'zajęcia'
+# g = ' z wizualizacji'
+# print(f + g)
+#
+# h = 7
+# i = 3
+# print(h // i)
+# print((2/3) ** i)
+# print(math.pow(2/3, i))
+#
+# h += 5
+# print(h)
 
-print(sys.version)
-a = 5
+a, b = 5, 3
+c = a - b
+print('wynik działania %(z1)d - %(z2)d = %(z3)d' %{'z1': a, 'z2': b, 'z3': c})
+print('wynik działania {0:d} = {1:d} = {2:d}'.format(a, b, c))
