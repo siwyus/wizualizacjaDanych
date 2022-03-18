b1a2bc03d347eddba34bf8c7f95a0156c251a7a0
=Szymon Jagaczewski <szymonjag2@gmail.com>
2022-03-18 12:49:39 +0100

    lab 3

--- main.py
+++ main.py
@@ -1,33 +1,70 @@
-# import sys
-# import math
-#
-# print(sys.version)
-# a = 'zajęcia\n z wizualizacji danych'
-# print(a)
-# print(type(a))
-# b, c = 5.4, 5
-# print(b)
-# print(type(b))
-# print(c)
-# print(type(c))
-# d = 3 + 2j
-# print(d)
-# print(type(d))
-#
-# f = 'zajęcia'
-# g = ' z wizualizacji'
-# print(f + g)
-#
-# h = 7
-# i = 3
-# print(h // i)
-# print((2/3) ** i)
-# print(math.pow(2/3, i))
-#
-# h += 5
-# print(h)
+def is_even(liczba):
+	return liczba % 2 == 0
 
-a, b = 5, 3
-c = a - b
-print('wynik działania %(z1)d - %(z2)d = %(z3)d' %{'z1': a, 'z2': b, 'z3': c})
-print('wynik działania {0:d} = {1:d} = {2:d}'.format(a, b, c))
+
+def show_exercise_number(number):
+	print('Zadanie %d' % number)
+
+
+# Zadanie 1
+show_exercise_number(1)
+A = [1 - x for x in range(1, 10)]
+B = [4 ** x for x in range(0, 7)]
+C = [x for x in B if is_even(x)]
+
+print(A)
+print(B)
+print(C)
+
+# Zadanie 2
+show_exercise_number(2)
+
+from numpy import random
+
+random_number_array = random.randint(100, size=10)
+
+only_even_numbers = [x for x in random_number_array if is_even(x)]
+
+print(only_even_numbers)
+
+# Zadanie 3
+show_exercise_number(3)
+by_piece = 'sztuki'
+products_dictionary = {'mąka': 'kg', 'chleb': by_piece, 'jajka': by_piece, 'szynka': 'kg'}
+
+only_products_by_piece = [key for key, value in products_dictionary.items() if value == by_piece]
+
+print(only_products_by_piece)
+
+# Zadanie 4
+show_exercise_number(4)
+
+
+def is_right_triangle(a, b, c):
+	return a ** 2 + b ** 2 == c ** 2
+
+
+print(is_right_triangle(3, 4, 5))
+
+# Zadanie 5
+show_exercise_number(5)
+
+
+def calc_trapezoid_field(base_top=5, base_bottom=10, height=7):
+	return (base_top + base_bottom) * height / 2
+
+
+print(calc_trapezoid_field())
+
+
+# Zadanie 6
+show_exercise_number(6)
+
+
+def calc_products_of_the_sequence(initial_value=1, quotient=4, elements_amount=10):
+	sequence = [initial_value]
+	for x in range(0, elements_amount - 1):
+		sequence.append(x * quotient)
+	print(sequence)
+
+calc_products_of_the_sequence()
